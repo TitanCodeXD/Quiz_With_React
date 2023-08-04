7a0e47f83b3b849be9c81e081c02758922870ebd
Wesley Santos <Wesleysantos32892653@outlook.com>
2023-08-03 21:01:51 -0300

    Criando o Component Welcome

---
+++ src/components/Welcome/Welcome.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Quiz from '../../img/quiz.svg';
+import './Welcome.css';
+
+const Welcome = () => {
+return (
+    <div id = "welcome">
+        <h2>Seja Bem-vindo</h2>
+        <p>Clique no botão abaixo para começar!</p>
+        <button>Iniciar</button>
+        <img src = {Quiz} alt = "Início do Quiz"/>
+    </div>
+)
+
+};
+
+
+export default Welcome
