2f9f51c8430456c1a8029f1dde2e0b55b6e1c38c
Wesley Santos <Wesleysantos32892653@outlook.com>
2023-08-04 22:40:51 -0300

    Criando o Componente Options e estilando as opções de resposta com css

--- src/components/Question/Question.jsx
+++ src/components/Question/Question.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useContext } from 'react';
 import { QuizContext } from '../../context/quiz';
+import Option from '../Option/Option';
 import './Question.css';
 
 
@@ -17,7 +18,9 @@ const Question = () => {
     <h2>{currentQuestion.question}</h2>
 
     <div id = "options-container">
-        <p>Opções</p>
+        {currentQuestion.options.map((option) => (
+        <Option option={option} key ={option}/>
+        ))}
     </div>
 
 <button onClick={() => dispatch({type: "CHANGE_QUESTION"})}>
